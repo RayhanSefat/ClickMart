7169e8a4f4e11ccd7739787f09c142f32eb3df49
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2025-01-13 16:04:44 +0600

    ProductRoute refactored

--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -15,15 +15,7 @@ router.post(
 
 router.get("/get-all-products-except-user/:username", productController.getAllProductsExceptUser);
 router.get("/get-products-by-user/:username", productController.getProductsByUser);
-router.get("/get-product-by-id/:id", async (req, res) => {
-  try {
-    console.log("API hitted successfully");
-    const product = await productController.getProductById(req.params.id);
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.get("/get-product-by-id/:id", productController.getProductById);
 router.put("/update-product/:id", productController.updateProduct);
 
 module.exports = router;
